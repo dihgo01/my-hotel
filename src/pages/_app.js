4cc991b2819a3173b31f33ccefc98baed6453e6a
dihgo01 <luka.poke9@gmail.com>
2021-11-02 22:35:32 -0300

    first commit

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+import GlobalStyles from '../styles/globals'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )}
+
+export default MyApp
